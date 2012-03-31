50be2ceec306b484cce89fb48e438f2ae69d15d7
Zsolt Fabok <me@zsoltfabok.com>
2012-03-31 10:54:07 +0300

    State after the forth episode

--- src/test/java/com/zsoltfabok/blog/SimpleTextMungerStepsdef.java
+++ src/test/java/com/zsoltfabok/blog/SimpleTextMungerStepsdef.java
@@ -1,42 +1,43 @@
 package com.zsoltfabok.blog;
 
 import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
 
 public class SimpleTextMungerStepsdef {
 
-    @Autowired
-    private SimpleTextMunger munger;
+    private SimpleTextMunger simpleTextMunger;
+    private Munger munger;
+    private SentenceHelper sentenceHelper;
     private String result;
+    private String inputWord;
+    private String mungedWord;
+
+    @Given("^I have a mocked munger which always returns \"([^\"]*)\" for \"([^\"]*)\"$")
+    public void I_have_a_mocked_munger_which_always_returns_(String munged, String word) {
+        munger = mock(Munger.class);
+        inputWord = word;
+        mungedWord = munged;
+    }
 
     @Given("^I have an instance of my class$")
     public void I_have_an_instance_of_my_class() {
+        sentenceHelper = new SentenceHelper();
+        simpleTextMunger = new SimpleTextMunger(sentenceHelper, munger);
     }
+
 
     @When("^I call my method with \"([^\"]*)\"$")
     public void I_call_my_method_with_(String word) {
-        result = munger.execute(word);
+        when(munger.munge(inputWord)).thenReturn(mungedWord);
+        result = simpleTextMunger.execute(word);
     }
 
     @Then("^I receive \"([^\"]*)\"$")
     public void I_receive_(String expectation) {
         assertEquals(expectation, result);
-    }
-
-    @Given("^the following users$")
-    public void the_following_users(List<Entry> entries) {
-        // iterate through the list and do something
-    }
-
-    class Entry {
-        String first_name;
-        String last_name;
     }
 }
--- src/test/resources/simple_text_munger.feature
+++ src/test/resources/simple_text_munger.feature
@@ -1,19 +1,9 @@
 Feature: simple text munger kata
 
   Background:
-    Given I have an instance of my class
-
-  Scenario Outline: It should munger a word properly
-    When I call my method with <input>
-    Then I receive <output>
-
-    Examples:
-     | input   | output  |
-     | "a"     | "a"     |
-     | "an"    | "an"    |
-     | "and"   | "and"   |
-     | "spice" | "scipe" |
+    Given I have a mocked munger which always returns "folw" for "flow"
+      And I have an instance of my class
 
   Scenario: It should process a sentence
-    When I call my method with "And the spice must flow"
-    Then I receive "And the scipe msut folw"
+    When I call my method with "flow flow"
+    Then I receive "folw folw"
